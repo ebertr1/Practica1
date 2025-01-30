a2cc1a9e05f5f0bdc89c3ef52272e55286b6b7c8
Eber_tr1 <eberson.guayllas@unl.edu.ec>
2025-01-29 21:33:20 -0500

    Final 100%

--- DiosBendiga/Front/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__, instance_relative_config=False)
-    with app.app_context():
-        from routes.router import router
-        app.register_blueprint(router)
-    return app
